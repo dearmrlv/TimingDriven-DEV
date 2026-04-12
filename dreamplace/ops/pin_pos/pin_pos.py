95563180e780217613bdabcb2f88823a0288b50e
lvzhengyang <lvzhengyang@mail.ustc.edu.cn>
2026-04-12 18:20:21 +0800

    repro: adapt baseline-only build for current CUDA toolchain

--- dreamplace/ops/pin_pos/pin_pos.py
+++ dreamplace/ops/pin_pos/pin_pos.py
@@ -12,9 +12,14 @@ from torch.autograd import Function
 
 import dreamplace.ops.pin_pos.pin_pos_cpp as pin_pos_cpp
 import dreamplace.configure as configure
+
 if configure.compile_configurations["CUDA_FOUND"] == "TRUE":
     import dreamplace.ops.pin_pos.pin_pos_cuda as pin_pos_cuda
-    import dreamplace.ops.pin_pos.pin_pos_cuda_segment as pin_pos_cuda_segment
+
+    try:
+        import dreamplace.ops.pin_pos.pin_pos_cuda_segment as pin_pos_cuda_segment
+    except ImportError:
+        pin_pos_cuda_segment = None
 
 import pdb
 
@@ -23,17 +28,31 @@ class PinPosFunction(Function):
     """
     @brief Given cell locations, compute pin locations.
     """
+
     @staticmethod
-    def forward(ctx, pos, pin_offset_x, pin_offset_y, pin2node_map,
-                flat_node2pin_map, flat_node2pin_start_map,
-                num_physical_nodes):
+    def forward(
+        ctx,
+        pos,
+        pin_offset_x,
+        pin_offset_y,
+        pin2node_map,
+        flat_node2pin_map,
+        flat_node2pin_start_map,
+        num_physical_nodes,
+    ):
         ctx.pos = pos.view(pos.numel())
         if pos.is_cuda:
             func = pin_pos_cuda.forward
         else:
             func = pin_pos_cpp.forward
-        output = func(ctx.pos, pin_offset_x, pin_offset_y, pin2node_map,
-                      flat_node2pin_map, flat_node2pin_start_map)
+        output = func(
+            ctx.pos,
+            pin_offset_x,
+            pin_offset_y,
+            pin2node_map,
+            flat_node2pin_map,
+            flat_node2pin_start_map,
+        )
         ctx.pin_offset_x = pin_offset_x
         ctx.pin_offset_y = pin_offset_y
         ctx.pin2node_map = pin2node_map
@@ -49,10 +68,16 @@ class PinPosFunction(Function):
             func = pin_pos_cuda.backward
         else:
             func = pin_pos_cpp.backward
-        output = func(grad_pin_pos.contiguous(), ctx.pos, ctx.pin_offset_x,
-                      ctx.pin_offset_y, ctx.pin2node_map,
-                      ctx.flat_node2pin_map, ctx.flat_node2pin_start_map,
-                      ctx.num_physical_nodes)
+        output = func(
+            grad_pin_pos.contiguous(),
+            ctx.pos,
+            ctx.pin_offset_x,
+            ctx.pin_offset_y,
+            ctx.pin2node_map,
+            ctx.flat_node2pin_map,
+            ctx.flat_node2pin_start_map,
+            ctx.num_physical_nodes,
+        )
         return output, None, None, None, None, None, None
 
 
@@ -60,18 +85,33 @@ class PinPosSegmentFunction(Function):
     """
     @brief Given cell locations, compute pin locations.
     """
+
     @staticmethod
-    def forward(ctx, pos, pin_offset_x, pin_offset_y, pin2node_map,
-                flat_node2pin_map, flat_node2pin_start_map,
-                num_physical_nodes):
+    def forward(
+        ctx,
+        pos,
+        pin_offset_x,
+        pin_offset_y,
+        pin2node_map,
+        flat_node2pin_map,
+        flat_node2pin_start_map,
+        num_physical_nodes,
+    ):
         ctx.pos = pos.view(pos.numel())
         if not pos.is_cuda:
             assert 0, "CPU version NOT implemented"
         else:
-            output = pin_pos_cuda_segment.forward(ctx.pos, pin_offset_x,
-                                                  pin_offset_y, pin2node_map,
-                                                  flat_node2pin_map,
-                                                  flat_node2pin_start_map)
+            assert pin_pos_cuda_segment is not None, (
+                "Segment pin_pos CUDA extension is unavailable"
+            )
+            output = pin_pos_cuda_segment.forward(
+                ctx.pos,
+                pin_offset_x,
+                pin_offset_y,
+                pin2node_map,
+                flat_node2pin_map,
+                flat_node2pin_start_map,
+            )
         ctx.pin_offset_x = pin_offset_x
         ctx.pin_offset_y = pin_offset_y
         ctx.pin2node_map = pin2node_map
@@ -88,10 +128,19 @@ class PinPosSegmentFunction(Function):
     def backward(ctx, grad_pin_pos):
         # grad_pin_pos is not contiguous
         if grad_pin_pos.is_cuda:
+            assert pin_pos_cuda_segment is not None, (
+                "Segment pin_pos CUDA extension is unavailable"
+            )
             output = pin_pos_cuda_segment.backward(
-                grad_pin_pos.contiguous(), ctx.pos, ctx.pin_offset_x,
-                ctx.pin_offset_y, ctx.pin2node_map, ctx.flat_node2pin_map,
-                ctx.flat_node2pin_start_map, ctx.num_physical_nodes)
+                grad_pin_pos.contiguous(),
+                ctx.pos,
+                ctx.pin_offset_x,
+                ctx.pin_offset_y,
+                ctx.pin2node_map,
+                ctx.flat_node2pin_map,
+                ctx.flat_node2pin_start_map,
+                ctx.num_physical_nodes,
+            )
         else:
             assert 0, "CPU version NOT implemented"
         if grad_pin_pos.is_cuda:
@@ -103,20 +152,23 @@ class PinPosSegmentFunction(Function):
 class PinPos(nn.Module):
     """
     @brief Given cell locations, compute pin locations.
-    Different from torch.index_add which computes x[index[i]] += t[i], 
+    Different from torch.index_add which computes x[index[i]] += t[i],
     the forward function compute x[i] += t[index[i]]
     """
-    def __init__(self,
-                 pin_offset_x,
-                 pin_offset_y,
-                 pin2node_map,
-                 flat_node2pin_map,
-                 flat_node2pin_start_map,
-                 num_physical_nodes,
-                 algorithm='segment'):
+
+    def __init__(
+        self,
+        pin_offset_x,
+        pin_offset_y,
+        pin2node_map,
+        flat_node2pin_map,
+        flat_node2pin_start_map,
+        num_physical_nodes,
+        algorithm="segment",
+    ):
         """
-        @brief initialization 
-        @param pin_offset pin offset in x or y direction, only computes one direction 
+        @brief initialization
+        @param pin_offset pin offset in x or y direction, only computes one direction
         @param algorithm segment|node-by-node
         """
         super(PinPos, self).__init__()
@@ -130,27 +182,39 @@ class PinPos(nn.Module):
 
     def forward(self, pos):
         """
-        @brief API 
-        @param pos cell locations. The array consists of x locations of movable cells, fixed cells, and filler cells, then y locations of them 
+        @brief API
+        @param pos cell locations. The array consists of x locations of movable cells, fixed cells, and filler cells, then y locations of them
         """
         assert pos.numel() % 2 == 0
         num_nodes = pos.numel() // 2
         if pos.is_cuda:
-            if self.algorithm == 'segment':
+            if self.algorithm == "segment":
                 return PinPosSegmentFunction.apply(
-                    pos, self.pin_offset_x, self.pin_offset_y,
-                    self.pin2node_map, self.flat_node2pin_map,
-                    self.flat_node2pin_start_map, self.num_physical_nodes)
+                    pos,
+                    self.pin_offset_x,
+                    self.pin_offset_y,
+                    self.pin2node_map,
+                    self.flat_node2pin_map,
+                    self.flat_node2pin_start_map,
+                    self.num_physical_nodes,
+                )
             else:
-                return PinPosFunction.apply(pos, self.pin_offset_x,
-                                            self.pin_offset_y,
-                                            self.pin2node_map,
-                                            self.flat_node2pin_map,
-                                            self.flat_node2pin_start_map,
-                                            self.num_physical_nodes)
+                return PinPosFunction.apply(
+                    pos,
+                    self.pin_offset_x,
+                    self.pin_offset_y,
+                    self.pin2node_map,
+                    self.flat_node2pin_map,
+                    self.flat_node2pin_start_map,
+                    self.num_physical_nodes,
+                )
         else:
-            return PinPosFunction.apply(pos, self.pin_offset_x,
-                                        self.pin_offset_y, self.pin2node_map,
-                                        self.flat_node2pin_map,
-                                        self.flat_node2pin_start_map,
-                                        self.num_physical_nodes)
+            return PinPosFunction.apply(
+                pos,
+                self.pin_offset_x,
+                self.pin_offset_y,
+                self.pin2node_map,
+                self.flat_node2pin_map,
+                self.flat_node2pin_start_map,
+                self.num_physical_nodes,
+            )
--- dreamplace/ops/utility/src/utils_cub.cuh
+++ dreamplace/ops/utility/src/utils_cub.cuh
@@ -11,11 +11,13 @@
 
 #include "utility/src/namespace.h"
 
-// include cub in a safe manner
+// include cub in a safe manner. This codebase relies on wrapping CUB inside
+// DREAMPLACE_NAMESPACE, which is compatible with the vendored CUB snapshot but
+// not with the newer CUB bundled in recent CUDA toolkits.
 #define CUB_NS_PREFIX namespace DREAMPLACE_NAMESPACE {
 #define CUB_NS_POSTFIX }
 #define CUB_NS_QUALIFIER DREAMPLACE_NAMESPACE::cub
-#include "cub/cub.cuh"
+#include "../../../../thirdparty/cub/cub/cub.cuh"
 #undef CUB_NS_QUALIFIER
 #undef CUB_NS_POSTFIX
 #undef CUB_NS_PREFIX
